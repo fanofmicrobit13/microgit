20a3328a7ebdbf14e8cf9086dd5c12ad12809df7
fanofmicrobit13 <128864616+fanofmicrobit13@users.noreply.github.com>
2023-03-24 18:10:54 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+basic.showString("Hello Microbits!")
+radio.sendString("Hello")
 basic.forever(function () {
 	
 })
